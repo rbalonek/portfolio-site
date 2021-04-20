569c78ca068a7e51b4974fde65fecf7ea5e1ace7
bob <robertbalonek@gmail.com>
2021-04-19 17:48:54 -0700

    Basic Outline

---
+++ src/screens/Projects/HappySpace/HappySpace.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HappySpace() {
+  return (
+    <div style={{ height: "200vh", backgroundColor: "black" }}>
+      <h1 style={{ color: "white", position: "sticky", top: 0 }}>
+        Happy Space
+      </h1>
+    </div>
+  );
+}
